f1ec1b09d893657c54922edfbf787a759082d147
BaquiranCharlesJustin <90014194+BaquiranCharlesJustin@users.noreply.github.com>
2024-03-14 12:24:27 +0800

    Read firebase data

--- app/src/main/java/com/example/sh_prototype/Data.kt
+++ app/src/main/java/com/example/sh_prototype/Data.kt
@@ -5,6 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.sh_prototype.databinding.FragmentDataBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,9 +27,13 @@ class Data : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: FragmentDataBinding
+    private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = FragmentDataBinding.inflate(layoutInflater)
+        databaseListener()
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -35,6 +46,20 @@ class Data : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_data, container, false)
+    }
+
+    private fun databaseListener(){
+        database = FirebaseDatabase.getInstance().getReference()
+        val postListener = object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val bpm = snapshot.child("MAX30102/BPM").value
+                binding.bpmData.setText(bpm.toString())
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        }
+        database.addValueEventListener(postListener)
     }
 
     companion object {
